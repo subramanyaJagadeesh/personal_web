df09d60ae301b2a448b3e0d40730f5cca1caabe0
subramanyajagadeesh <subramanya.grj@gmail.com>
2024-09-09 11:19:00 -0700

    intro css change

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -10,19 +10,23 @@ const Intro = () => {
           <img src={profile} alt="Subramanya Jagadeesh" />
         </div>
         <div className="right">
-          <h2>
-            Hi, I'm <span id="name">Subramanya Jagadeesh</span><span id="period">.</span>
-          </h2>
-          <h3>
-            Full-Stack Software Engineer with a knack for efficiency and scalability.
-          </h3>
-          <div className="box" />
-          <h4>
-            With a rich background in both front-end and back-end technologies, I have rapidly contributed to and enhanced multiple high-scale projects. My expertise spans developing resilient APIs to optimizing user interfaces for enhanced performance. I excel in environments that challenge my abilities and allow for growth through cutting-edge technologies.
-          </h4>
-          <a href="https://www.linkedin.com/in/subramanyajagadeesh" target="_blank" rel="noopener noreferrer" className="linkedin-button">
-            Connect on LinkedIn
-          </a>
+          <div>
+            <h2>
+              Hi, I'm <span id="name">Subramanya Jagadeesh</span><span id="period">.</span>
+            </h2>
+            <h3>
+              Full-Stack Software Engineer with a knack for efficiency and scalability.
+            </h3>
+            <div className="box" />
+          </div>
+          <div>
+            <h4>
+              With a rich background in both front-end and back-end technologies, I have rapidly contributed to and enhanced multiple high-scale projects. My expertise spans developing resilient APIs to optimizing user interfaces for enhanced performance. I excel in environments that challenge my abilities and allow for growth through cutting-edge technologies.
+            </h4>
+            <a href="https://www.linkedin.com/in/subramanyajagadeesh" target="_blank" rel="noopener noreferrer" className="linkedin-button">
+              Connect on LinkedIn
+            </a>
+          </div>
         </div>
       </div>
     </div>
