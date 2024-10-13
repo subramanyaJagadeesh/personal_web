5e98644c8503339b158bb10572c9e619ca00600f
subramanyajagadeesh <subramanya.grj@gmail.com>
2024-10-13 00:01:45 -0700

    Updated portfolio

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Pulse } from './Pulse';
-import Skills from './Skills';
 
 const list = [
   {
@@ -24,7 +23,9 @@ const list = [
       "Led the migration of a service from a monolithic architecture to a Java Spring Boot microservice, reducing API response times from 1.5s to under 200ms.",
       "Optimized SQL queries and reduced execution times significantly, improving system performance.",
       "Enhanced the organization's web application (React) through refactoring, optimizing computations, memoizing data, and implementing code-splitting and lazy loading techniques, resulting in a significant reduction of 30% (2s) in load times.",
-      "Led the unit testing efforts for the frontend (react-testing) and backend (JUnit) applications thereby covering 90% of the codebase, further enhancing the robustness of the application."
+      "Led the unit testing efforts for the frontend (react-testing) and backend (JUnit) applications thereby covering 90% of the codebase, further enhancing the robustness of the application.",
+      "Conducted code reviews, improving code quality and ensuring best practices for front-end and back-end integration.",
+      "Worked closely with junior engineers, providing mentorship and technical guidance.",
     ]
   },
   {
@@ -33,8 +34,10 @@ const list = [
     company: "Khoros",
     role: "Software Engineer",
     tasks: [
-      "Learned Scala and developed a suite of efficient APIs with Finatra, GraphQL and PostgreSQL within 2 weeks, further deployed them on containers using Docker orchestrated via Kubernetes.",
-      "Worked with a team of 2 to translate legacy JS features into fast, robust and responsive Typescript React"
+      "Learnt Scala and started contributing to the codebase in mere weeks.",
+      "Built enhancements and fixed critical bug on 2 of the company's products.",
+      "Built and delivered high-performance REST APIs using Finatra, GraphQL, and PostgreSQL for a globally distributed system, improving data access speed by 50%, deployed them on Docker containers orchestrated by Kubernetes.",
+      "Migrated an application used by 2000 daily active users from legacy JavaScript to ES6 TypeScript React + Redux thereby improving load times and enriching user experience."
     ]
   },
   {
@@ -43,12 +46,10 @@ const list = [
     company: "Royal Brothers",
     role: "Full-Stack Developer",
     tasks: [
-      "Learned new frameworks like React and Rails in a mere couple weeks.",
       "Worked on the whole stack of the company's web app (React (UI), Rails(APIs), Postgres).",
       "Developed and tested robust and performance-oriented features for the web app.",
       "Also had the opportunity to build the company's mobile apps, built on React-Native.",
       "In the beginning, solely contributed to the development, testing, and publishing of the said mobile apps.",
-      "Had a thorough understanding of the company's products in less than 2 months."
     ]
   },
   {
@@ -57,11 +58,11 @@ const list = [
     company: "Woke Cartel",
     role: "Co-Founder",
     tasks: [
-      "Learned frameworks like Vue and Express in a short amount of time.",
+      "Learned frameworks like Nuxt.js and Express in a short amount of time.",
       "Developed, tested and published 2 main products for the company.",
       "1. A Content Management System (CMS) to provide CRUD operations on data that it provides.",
       "2. A digital sports media website, which receives its content from the CMS.",
-      "Deployed the 2 applications on Digital Ocean, with the help of nginx and pm2."
+      "Deployed the 2 applications on Digital Ocean for the frontend and Firebase functions for the backend."
     ]
   }
 ];
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,29 +3,35 @@ import { Pulse } from './Pulse'; // Assuming Pulse is a component used for anima
 
 const projects = [
   {
+    title: "TaxGPT",
+    description: "Developed a tax assistant application using Ollama, where I designed a FastAPI backend to handle communication with the AI model. Additionally, built a user interface using Vanilla JS, HTML, CSS3, and Bootstrap, providing a smooth, user-friendly experience. To maintain conversational continuity, Integrated a context storage system in an in-memory SQLite database, allowing users to return to previous interactions and pick up the conversation seamlessly. This project demonstrated my ability to create cohesive full-stack applications with memory management and context-aware AI integrations.",
+    year: "2024",
+    link: "https://github.com/subramanyaJagadeesh/taxGPT"
+  },
+  {
     title: "Distributed Task Scheduler",
-    description: "Built the scheduling algorithm in C++ for a task scheduler that allocates work across distributed nodes on a network.",
+    description: "Led the design and development of a scheduling algorithm in C++ for a distributed task scheduler in a class-wide project. Provided foundational architecture to peers, guiding the system's build and ensuring effective task distribution across network nodes. Collaborated with classmates on integrating the scheduler, iterating on design feedback to achieve efficient load balancing and scalability. Successfully implemented the scheduler, demonstrating reliable performance across distributed nodes, contributing to the overall success of the system.",
     year: "2024",
     link: "https://github.com/275-challenge-project-2024/scheduler-algorithm"
   },
   {
     title: "Paxos with Neurelo",
-    description: "Implemented Multi-Decree Paxos protocol in Python leveraging Neurelo’s Data Access APIs to store and retrieve messages in a remote Postgres Instance.",
+    description: "In a personal project, Implemented the Multi-Decree Paxos protocol using Python, integrating Neurelo's Data Access APIs to facilitate message storage and retrieval from a remote PostgreSQL instance. The challenge was in understanding the nuances of the Paxos protocol and adapting it for efficient communication with a remote database, which acted as a message queue. This experience honed my problem-solving skills as I navigated complex distributed systems and ensured robust message handling across nodes using the PostgreSQL queue.",
     year: "2024",
     link: "https://github.com/subramanyaJagadeesh/paxos-with-neurelo"
   },
   {
     title: "Theatre Management",
-    description: "Built a theatre management system with 3 group members. Solely deployed the entire application to AWS; Flask Server on EC2 (ELB attached) and React app on S3.",
+    description: "Led the architecture and deployment of a theatre management system, working with three group members. Further, deployed the Flask server on an AWS EC2 instance, attaching an ELB for load balancing, and hosted the React frontend on S3. Took charge of developing the frontend application while mentoring a teammate in React.js. Additionally, Contributed to the backend by designing and implementing several key APIs. This project demonstrated my leadership, technical skills, and ability to collaborate in a team environment.",
     year: "2023",
     link: "https://github.com/subramanyaJagadeesh/teamproject-nullpointer"
   },
   {
     title: "Consistent hashing",
-    description: "Designed a distributed consistent hashing system with ring structure rebalancing to provide fault-tolerance",
+    description: "Led the design and architecture of a distributed consistent hashing system with a ring structure, ensuring fault-tolerance and efficient rebalancing of nodes in the network. In addition to the architecture, developed the gateway responsible for managing communication between all the nodes. Also implemented the gRPC communication protocol, allowing for seamless interaction and data transfer between the distributed components. This project highlighted my ability to design fault-tolerant systems and leverage distributed computing principles to achieve scalable and resilient architectures.",
     year: "2023",
     link: "https://github.com/subramanyaJagadeesh/consistent_hashing_cmpe_273"
-  },
+  }
 ];
 
 const Projects = () => {
